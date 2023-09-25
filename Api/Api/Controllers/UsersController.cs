3518864e1e376c4d3a084995bbc3e7d8813aeeb0
kirill <zinchenkokir@gmail.com>
2023-09-25 14:27:55 +0300

    addDB

---
+++ Api/Api/Controllers/UsersController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Api.DataContext;
+using Api.Models.Entity;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UsersContext _context;
+
+        public UsersController(UsersContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet, Route("List")]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+          if (_context.Users == null)
+          {
+              return NotFound();
+          }
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet, Route("Get")]
+        public async Task<ActionResult<User>> GetUser(string Email)
+        {
+          if (_context.Users == null)
+          {
+              return NotFound();
+          }
+            var user = await _context.Users.FirstOrDefaultAsync(e => e.Email == Email);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPut, Route("Update")]
+        public async Task<IActionResult> PutUser(long id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost, Route("Create")]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+          if (_context.Users == null)
+          {
+              return Problem("Entity set 'UsersContext.Users'  is null.");
+          }
+            _context.Users.Add(user);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (UserExists(user.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        [HttpDelete, Route("Delete")]
+        public async Task<IActionResult> DeleteUser(long id)
+        {
+            if (_context.Users == null)
+            {
+                return NotFound();
+            }
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(long id)
+        {
+            return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Api/Api/Models/Entity/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Api.Models.Entity;
+
+public partial class User
+{
+    public long Id { get; set; }
+
+    public string? NickName { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string? Comments { get; set; }
+
+    public DateTime CreateDate { get; set; }
+}
